e8803d50dd2d52bda37b9cc591923ed69530b454
songusika <sws981202@gmail.com>
2023-12-21 18:41:50 +0900

    feat: 출결 도메인 작성

---
+++ backend/src/main/kotlin/com/woowahan/campus/attendace/domain/Attendance.kt
@@ -0,0 +1,23 @@
+package com.woowahan.campus.attendace.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDate
+
+@Entity
+class Attendance(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private var id: Long = 0L,
+    private var crewId: Long,
+    @Column(nullable = false)
+    private var date: LocalDate,
+    @Embedded
+    private var checkIn: CheckIn,
+    @Embedded
+    private var checkOut: CheckOut,
+)
